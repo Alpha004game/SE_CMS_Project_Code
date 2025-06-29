7cb13e25be39d836c759367254c9b9dd4444faa3
Alpha004game <alpha004thegamer@gmail.com>
2025-06-29 16:11:02 +0200

    a

--- src/main/java/com/cms/App.java
+++ src/main/java/com/cms/App.java
@@ -1,14 +1,19 @@
 package com.cms;
 
+import com.cms.users.Commons.DBMSBoundary;
+import com.cms.users.account.Interface.LoginScreen;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
+    public final static DBMSBoundary dbms=new DBMSBoundary();
     public static void main( String[] args )
     {
         System.out.println( "Hello gays!" );
-        
+        LoginScreen login=new LoginScreen();
+        login.create();
     }
 }
